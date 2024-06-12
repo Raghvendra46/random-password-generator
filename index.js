b2016ee2e64ab13b436e61d7135d715e9f098bef
Raghvendra Singh Chauhan <52023688+Raghvendra46@users.noreply.github.com>
2024-06-12 22:52:25 +0530

    Update index.js
    Removed Semicolons at 3 lines to ensure consistent format.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const generatePasswordsBtn = document.getElementById("generate-passwords")
 
 
 // Declaring arrays with Characters
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?"]
 
@@ -22,15 +22,15 @@ generatePasswordsBtn.addEventListener("click", generatePassword)
 
 // To Copy firstPassword to Clipboard
 firstPassword.addEventListener("click", function () {
-    const clipText1  = firstPassword.textContent;
-    navigator.clipboard.writeText(clipText1);
+    const clipText1  = firstPassword.textContent
+    navigator.clipboard.writeText(clipText1)
 })
 
 
 // To Copy secondPassword to Clipboard
 secondPassword.addEventListener("click", function () {
-    const clipText2  = secondPassword.textContent;
-    navigator.clipboard.writeText(clipText2);
+    const clipText2  = secondPassword.textContent
+    navigator.clipboard.writeText(clipText2)
 })
 
 
